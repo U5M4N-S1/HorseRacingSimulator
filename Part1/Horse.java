e6da047f80f0a0479767925313c1d7ab9df8186d
U5M4N-S1 <siddiquiusman2005@gmail.com>
2025-04-27 15:14:19 +0100

    Updated folder structure to implement part1 and 2

---
+++ Part1/Horse.java
@@ -0,0 +1,84 @@
+
+/**
+ * Horse data will be handled here
+ * 
+ * @author Usman Siddiqui
+ * @version 1.0
+ */
+public class Horse
+{
+    //Fields of class Horse
+    private String horseName;
+    private char horseSymbol;
+    private double horseConfidence;
+    private int distanceTravelled = 0;
+    private boolean hasFallen = false;
+      
+    //Constructor of class Horse
+    /**
+     * Constructor for objects of class Horse
+     */
+    public Horse(char horseSymbol, String horseName, double horseConfidence)
+    {
+        this.horseName = horseName;
+        this.horseSymbol = horseSymbol;
+        this.horseConfidence = horseConfidence;
+    }
+    
+    
+    //Other methods of class Horse
+    public void fall()
+    {
+        this.setConfidence(this.horseConfidence -= 0.05);
+        this.hasFallen = true;
+    }
+    
+    public double getConfidence()
+    {
+        return this.horseConfidence;
+    }
+    
+    public int getDistanceTravelled()
+    {
+        return this.distanceTravelled;
+    }
+    
+    public String getName()
+    {
+        return this.horseName;
+    }
+    
+    public char getSymbol()
+    {
+        return this.horseSymbol;
+    }
+    
+    public void goBackToStart()
+    {
+        this.distanceTravelled = 0;
+    }
+    
+    public boolean hasFallen()
+    {
+        return this.hasFallen;
+    }
+
+    public void moveForward()
+    {
+        this.distanceTravelled += 1;
+    }
+
+    public void setConfidence(double newConfidence)
+    {
+        if (newConfidence > 1 || newConfidence < 0)
+            return;
+        else
+            this.horseConfidence = newConfidence;
+    }
+    
+    public void setSymbol(char newSymbol)
+    {
+        this.horseSymbol = newSymbol;
+    }
+    
+}
